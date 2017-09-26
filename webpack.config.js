03324428c9f48bd8f2b1e78236d20445ba21eae1
Gerardnico <gerardnico@gerardnico.com>
2017-09-26 23:33:40 +0200

    The debug session now works with an attach session

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const host = process.env.HOST || 'localhost';
 
 const paths = {
     src: path.join(__dirname, 'src'),
@@ -9,22 +10,52 @@ const paths = {
 }
 
 module.exports = {
-    // The entry key describes the entry file where the scripts and styles should be imported to
+    // These are the "entry points" to our application.
+    // This means they will be the "root" imports that are included in JS bundle.
     entry: ['./src/index.js'],
     // The output key describes the wanted output
     output: {
         filename: 'bundle.js',
+        // The build path
         path: paths.dist,
+        // Add /* filename */ comments to generated require()s in the output.
         pathinfo: true,
-        sourceMapFilename: '[file].map'
+        sourceMapFilename: '[file].map',
+        // Point sourcemap entries to original disk location
+        devtoolModuleFilenameTemplate: info =>
+            path.resolve(info.absoluteResourcePath)
     },
     // The dev server description
     devServer: {
+        // By default WebpackDevServer serves:
+        //      * physical files from the current directory
+        //      * the virtual build files from memory.
+        // The contentBase directory defines the location of the physical files that must be served
         contentBase: paths.dist,
+        // By default files from `contentBase` will not trigger a page reload.
+        watchContentBase: true,
+        // Enable gzip compression of generated files.
         compress: true,
-        port: 9000
+        // Silence WebpackDevServer's own logs since they're generally not useful.
+        // It will still show compile warnings and errors with this setting.
+        clientLogLevel: 'none',
+        // Reportedly, this avoids CPU overload on some systems.
+        // https://github.com/facebookincubator/create-react-app/issues/293
+        watchOptions: {
+            ignored: /node_modules/,
+        },
+        port: 9000,
+        host: host,
+        overlay: false,
+        historyApiFallback: {
+            // Paths with dots should still use the history fallback.
+            // See https://github.com/facebookincubator/create-react-app/issues/387.
+            disableDotRule: true,
+        }
     },
-    devtool: 'inline-source-map', // or devtool: 'inline-source-map' or 'source-map'
+    // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
+    // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
+    devtool: 'cheap-module-source-map', // or devtool: 'inline-source-map' or 'source-map'
     plugins: [
         // Copy the data
         new CopyWebpackPlugin([
@@ -36,12 +67,12 @@ module.exports = {
         // // https://webpack.js.org/guides/migrating/#debug
         new webpack.LoaderOptionsPlugin({
             debug: true
-        }),
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[file].map',
-            exclude: ['vendors.js'],
-            columns: false, // no columns in SourceMaps
-            module: false / true // use SourceMaps from loaders 
-         })
+        })
     ],
+    // Turn off performance hints during development because we don't do any
+    // splitting or minification in interest of speed. These warnings become
+    // cumbersome.
+    performance: {
+        hints: false,
+    },
 }
